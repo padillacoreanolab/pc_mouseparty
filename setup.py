43547566d1fefacf2eae9db48f34198ffa799202
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2023-08-09 16:13:00 -0400

    testing publishing of new version

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/padillacoreanolab/pc_mouseparty',
-    version='version='version='version='version='version='version='0.0.2''''''',
+    version='0.0.2',
     zip_safe=False,
 )
