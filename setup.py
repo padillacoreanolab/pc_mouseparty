afbaeee9711ef2fb7e40db9c039e1287740da364
PadillaCoreanoLabGeneral <padillacoreanolab@gmail.com>
2023-08-10 20:59:18 +0000

    Bump version: 0.0.19 → 0.0.20

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/padillacoreanolab/pc_mouseparty',
-    version='0.0.19',
+    version='0.0.20',
     zip_safe=False,
 )
