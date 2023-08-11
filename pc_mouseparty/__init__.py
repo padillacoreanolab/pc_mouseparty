e5d782faae6e6e352576e7cf73651b838e6aeef1
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2023-08-10 22:53:28 -0400

    __init__ test 3

--- pc_mouseparty/__init__.py
+++ pc_mouseparty/__init__.py
@@ -18,20 +18,32 @@ __version__ = '0.0.22'
 # for module in __all__:
 #     exec(f"from .{module} import *")
 
+# import os
+# import pkgutil
+
+# __all__ = []
+
+# for finder, name, ispkg in pkgutil.walk_packages(__path__):
+#     if ispkg:
+#         __all__.append(name)
+#         import_module = f'.{name}'
+#     else: 
+#         import_module = f'.{name}'
+#         __all__.append(name.split('.')[0])
+    
+#     try:
+#         globals()[name] = __import__(import_module, globals(), locals(), [], 0)
+#     except ModuleNotFoundError:
+#         pass
+
 import os
 import pkgutil
-
-__all__ = []
+import importlib
 
 for finder, name, ispkg in pkgutil.walk_packages(__path__):
     if ispkg:
-        __all__.append(name)
-        import_module = f'.{name}'
-    else: 
-        import_module = f'.{name}'
-        __all__.append(name.split('.')[0])
-    
-    try:
-        globals()[name] = __import__(import_module, globals(), locals(), [], 0)
-    except ModuleNotFoundError:
-        pass
+        import_path = f'{name}'
+    else:
+        import_path = f'{name}'
+
+    importlib.import_module(import_path, __package__)
